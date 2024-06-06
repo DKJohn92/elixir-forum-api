ee0358ae24c2d9ee191a31651a5bf713b5bd22fe
kerschl <kerschl@publicplan.de>
2024-06-06 18:23:51 +0200

    feat: 🎸 add api route for users and posts

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,13 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "Steve", email: "steve@email.com"},
+      %{id: 3, name: "Max", email: "max@email.com"}
+    ]
+    json(conn, %{users: users})
+  end
 end
